91fa1e83df8256d78949384fe9479afdfe4c50f5
Faizan <alvi83252@gmail.com>
2026-02-12 22:41:01 +0000

    COMMIT

---
+++ Labs/Week 20 Lab/Backend/eventfinder/src/test/java/startup1/eventfinder/EventfinderApplicationTests.java
@@ -0,0 +1,13 @@
+package startup1.eventfinder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventfinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
